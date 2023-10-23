77fe61907b704c10377b6e807b625b8caf273790
WegielWBucie <pondroszejset@gmail.com>
2023-10-24 01:47:28 +0200

    Lab 2 do oceny.

---
+++ oolab/src/main/java/agh/ics/oop/model/Vector2D.java
@@ -0,0 +1,69 @@
+package agh.ics.oop.model;
+
+public class Vector2D {
+    private final int x;
+    private final int y;
+
+    public Vector2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public boolean precedes(Vector2D other) {
+        return this.x <= other.getX() && this.y <= other.getY();
+    }
+
+    public boolean follows(Vector2D other) {
+        return this.x >= other.getX() && this.y >= other.getY();
+    }
+
+    public Vector2D add(Vector2D other) {
+        return new Vector2D(this.x + other.getX(), this.y + other.getY());
+    }
+
+    public Vector2D subtract(Vector2D other) {
+        return new Vector2D(this.x - other.getX(), this.y - other.getY());
+    }
+
+    public Vector2D upperRight(Vector2D other) {
+        return new Vector2D(
+                Math.max(this.x, other.getX()), Math.max(this.y, other.getY())
+        );
+    }
+
+    public Vector2D lowerLeft(Vector2D other) {
+        return new Vector2D(
+                Math.min(this.x, other.getX()), Math.min(this.y, other.getY())
+        );
+    }
+
+    public Vector2D opposite() {
+        return new Vector2D(-this.x, -this.y);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (!(other instanceof Vector2D that))
+            return false;
+        return this.x == that.getX() && this.y == that.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
